f0453ddd05ff41ac6c5a75aa3c4acf8e244100cf
Cheng-Han Yu <cheyu@soe.ucsc.edu>
2017-07-18 22:39:41 -0700

    add new function times

---
+++ R/times.R
@@ -0,0 +1,11 @@
+#' Multiply together two numbers
+#'
+#' @param x A number
+#' @param y A number
+#' @return The product of \code{x} and \code{y}
+#' @examples
+#' times(1, 1)
+#' times(10, 1)
+times <- function(x, y) {
+    x * y
+}
